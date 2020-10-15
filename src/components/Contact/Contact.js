9ea23bb85787baae984ef3655c0c940ca1c5f74e
faustc908 <57776835+faustc908@users.noreply.github.com>
2020-10-15 18:34:55 -0400

    updates

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,7 +5,7 @@ import { FaGithub, FaLinkedin, FaMailBulk } from "react-icons/fa";
 const Contact = () => {
     return (
         <div class='skills'>
-            <h4>If you want to reach out or seeing other work:</h4>
+            <h4>If you want to reach out or see other work:</h4>
             <p className='heading'>Email LinkedIn Github</p>
             <a className='links' href='mailto:faust.colint@gmail.com'><FaMailBulk /></a>
             <a className='links' href='https://www.linkedin.com/in/colin-faust-237077100?trk=people-guest_profile-result-card_result-card_full-click' target="_blank"><FaLinkedin /></a>
