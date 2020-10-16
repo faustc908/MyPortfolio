be1d3f0f9b72f5a0236bf8b1752013e7322b6261
faustc908 <57776835+faustc908@users.noreply.github.com>
2020-10-16 00:35:12 -0400

    updates

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -10,7 +10,7 @@ const Body = () => {
             </div>
             <div className="aboutme">
                 <h1>Skills and experience</h1>
-                <p>  I'm proficient in HTML5/CSS3/JS/JQuery as well as the PERN stack (React.js/Node.js/PostgreSQL/Express).  For application and server testing I generally use Jest/Supertest/Enzyme.  I create my code from scratch and have a deep knowledge of front end design.  I have currently deployed two fullstack applications I made from scratch using Heroku for the server and Vercel for the client.  I've been in leadership positions in my previous career and have customer service and sales skills as well.  I am always looking for ways to improve and learn more.  I would really love to work on a team of passionate programmers and grow my career among like-minded people.
+                <p>  I'm proficient in HTML5/CSS3/JS/JQuery as well as the PERN stack (React.js/Node.js/PostgreSQL/Express).  For application and server testing I generally use Jest/Supertest/Enzyme.  I create my code from scratch and have a deep knowledge of front end design.  I have currently deployed two fullstack applications I made from scratch using Heroku for the server and Vercel for the client.  I've been in leadership positions in my previous career and have customer service and sales skills as well.  I'm always looking for ways to improve and learn more.  I would really love to work on a team of passionate programmers and grow my career among like-minded people.
                </p>
             </div>
         </div>
