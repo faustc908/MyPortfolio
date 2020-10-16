9e5e5120c340965532498f845875afb5d282cfe9
faustc908 <57776835+faustc908@users.noreply.github.com>
2020-10-16 00:01:38 -0400

    updates

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,8 +6,8 @@ const Contact = () => {
     return (
         <div class='skills'>
             <h4>If you want to reach out or see other work:</h4>
-            <p className='heading' aria-label="Email Link">Email LinkedIn Github</p>
-            <a className='links' href='mailto:faust.colint@gmail.com'><FaMailBulk /></a>
+            <p className='heading' >Email LinkedIn Github</p>
+            <a className='links' aria-label="Email Link" href='mailto:faust.colint@gmail.com'><FaMailBulk /></a>
             <a className='links' aria-label="LinkedIn link" href='https://www.linkedin.com/in/colin-faust-237077100?trk=people-guest_profile-result-card_result-card_full-click' target="_blank"><FaLinkedin /></a>
             <a className='links' aria-label="Github link" href='https://github.com/faustc908' target="_blank"><FaGithub /></a>
         </div>
