ff7944647be0b709497384c6a04a944146d7e4dc
faustc908 <57776835+faustc908@users.noreply.github.com>
2020-10-17 18:35:47 -0400

    updates

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -12,7 +12,7 @@ const Projects = () => {
                 <h1 className="header">Some of my projects</h1>
                 <h2>First Fullstack Application - GoodEats Recipe Finder</h2>
                 <img className="goodEats" src={logo} alt="logo ExpenseApp expense keeper" />
-                <p>A fullstack application using the PERN (React.js, Node.js, PostgreSQL, Express) stack to create an app that lets you search for recipes by ingredient and also has the ability to record user recipes.  Tested with Jest, Supertest, and Enzyme.  This is an upgraded application from my jQuery one for the biggest foodies and uses a different recipe database then my previous one to make third party calls.</p>
+                <p>A full stack application using the PERN (React.js, Node.js, PostgreSQL, Express) stack to create an app that lets you search for recipes by ingredient and also has the ability to record user recipes.  Tested with Jest, Supertest, and Enzyme.  This is an upgraded application from my jQuery one for the biggest foodies and uses a different recipe database then my previous one to make third party calls.</p>
                 <div>
                     <a className="link" href='https://good-eats-recipe-client.vercel.app/' target="_blank">Link to live app</a>
                     <a className="link" href='https://github.com/faustc908/GoodEats-Recipe-Client' target="_blank">Client repo for app</a>
@@ -23,7 +23,7 @@ const Projects = () => {
             <div className='projects'>
                 <h2>Second Fullstack Application - ExpenseApp</h2>
                 <img className="goodEats" src={logoThree} alt="logo GoodEats recipe finder React.js/Node.js" />
-                <p>A fullstack application using the PERN (React.js, Node.js, PostgreSQL, Express) stack to create an app that lets you caculate your expenses as well as leave notes on different deals and nuances in expenditures.  Tested with Jest, Supertest, and Enzyme.  This application was developed with the financial hardship covid-19 has placed on people and should be a useful tool.</p>
+                <p>A full stack application using the PERN (React.js, Node.js, PostgreSQL, Express) stack to create an app that lets you calculate your expenses as well as leave notes on different deals and nuances in expenditures.  Tested with Jest, Supertest, and Enzyme.  This application was developed with the financial hardship covid-19 has placed on people and should be a useful tool.</p>
                 <div>
                     <a className="link" href='https://expense-app-client.vercel.app/' target="_blank">Link to live app</a>
                     <a className="link" href='https://github.com/faustc908/ExpenseApp-Client' target="_blank">Client repo for app</a>
